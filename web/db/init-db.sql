59d4f04b34d4a5a701e79ae113e34aa93a3635b5
Paolo Palumbo <paol.palumbo@gmail.com>
2025-10-27 15:39:03 +0100

    Set default value for company_description column in users table

--- web/db/init-db.sql
+++ web/db/init-db.sql
@@ -3,7 +3,7 @@ CREATE TABLE IF NOT EXISTS users (
     id SERIAL PRIMARY KEY,
     username VARCHAR(100) NOT NULL UNIQUE,
     password_hash TEXT NOT NULL,
-    company_description TEXT
+    company_description TEXT DEFAULT ''
 );
 
 -- Projects table
